8df5b932bbff722c13eb20ceed005a2bbd352bb8
RKM26 <rishabhkumarmayank@gmail.com>
2022-01-16 18:47:48 +0530

    routing | dynamic routing

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 function ClientsPage() {
+
+  const clients = [
+    { id: "max", name : "maxmilian"},
+    { id: "manu", name : "manual"}
+  ];
     return (
       <div>
-        <h1> The Clients Page</h1>
+        <h1> The Clients Page 1</h1>
+        <ul>
+          {console.log(clients)}
+          {clients && clients.map((client)=>(
+            <li key={client.id}>
+              <Link href={{
+                pathname: '/clients/[id]/[clientprojectid]',
+                query: { id: "hi" ,clientprojectid : client.id}
+              }}>{client.name}</Link>
+            </li>
+          ))}
+        {/* <li>
+          <Link href="/clients/max">max</Link>
+        </li>
+        <li>
+          <Link href="/clients/manu">manu</Link>
+        </li> */}
+      </ul>
       </div>
     );
   }
